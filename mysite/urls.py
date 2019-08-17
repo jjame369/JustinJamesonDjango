264403a30883cd3fefd7a26f2fd9e45713628dd4
Justin Jameson <jjameson@uw.edu>
2019-08-17 16:31:07 -0700

    Stubbed out

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('bloggin.urls')),
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
 ]
