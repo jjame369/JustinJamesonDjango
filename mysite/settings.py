7db5e8170fe381ed55744637cca4a173e579cccd
Justin Jameson <jjameson@uw.edu>
2019-08-18 10:19:06 -0700

    Starting week 9, deployment to Azure

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'polling',
     'bloggin',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.github',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+SITE_ID = 1
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,10 +19,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
+    url(r'^accounts/', include('allauth.urls')),
     path('', include('bloggin.urls')),
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+
 
 ]
